0c8206c6a0419af7f955bdf322121102a5b277b2
odalton <oliver.dalton.au@gmail.com>
2014-07-28 20:24:36 +0800

    Added phone number and ga tracking code.

--- sites/default/modules/hbc_header/templates/hbc_header_contact.tpl.php
+++ sites/default/modules/hbc_header/templates/hbc_header_contact.tpl.php
@@ -1,6 +1,6 @@
 <div class="hbc-contact-information">
   <ul>
-    <li><i class="icon-phone"></i><a class="hbc-contact-tel" href="tel:+13174562564">317-456-2564</a></li>
+    <li><i class="icon-phone"></i><a class="hbc-contact-tel" href="tel:+13174562564">1300 760 376</a></li>
     <li><i class="icon-envelope"></i><a class="hbc-contact-email" href="mailto:info@hedlandbusinesscentre.com.au">info@hedlandbusinesscentre.com.au</a></li>
   </ul>
 </div>
